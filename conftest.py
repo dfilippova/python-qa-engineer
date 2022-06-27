c00da11ae5d8227a300e2c9f5d8b24752ff663f1
dfilippova <dariasharando@gmail.com>
2022-06-28 00:02:04 +0400

    Add logging

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
+import logging
 import os
+from datetime import datetime
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.opera.options import ChromeOptions
@@ -9,28 +12,75 @@ def pytest_addoption(parser):
     parser.addoption('--browser', default='chrome')
     parser.addoption('--drivers', default=os.path.expanduser('~/Develop/drivers'))
     parser.addoption('--url', default='http://192.168.2.113')
+    parser.addoption('--log_level', default='DEBUG')
+    parser.addoption('--executor', default='192.168.2.113')
+    parser.addoption('--vnc', action='store_true')
+    parser.addoption('--browser_version')
 
 
 @pytest.fixture(scope='module')
 def driver(request):
     browser_name = request.config.getoption('--browser')
     drivers = request.config.getoption('--drivers')
+    log_level = request.config.getoption('--log_level')
+    executor = request.config.getoption('--executor')
+    vnc = request.config.getoption('--vnc')
+    browser_version = request.config.getoption('--browser_version')
 
-    if browser_name == 'chrome':
-        browser = webdriver.Chrome(executable_path=f'{drivers}/chromedriver')
-    elif browser_name == 'firefox':
-        browser = webdriver.Firefox(executable_path=f'{drivers}/geckodriver')
-    elif browser_name == 'opera':
-        options = ChromeOptions()
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler('logs.log')
+    file_handler.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s : %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+
+    logger.info(f'Тесты "{request.node.name}" начали прохождение в {datetime.now()}')
+
+    options = ChromeOptions()
+    if browser_name == 'opera':
         options.add_experimental_option('w3c', True)
-        browser = webdriver.Opera(executable_path=OperaDriverManager().install(), options=options)
-    elif browser_name == 'safari':
-        browser = webdriver.Safari(executable_path='/usr/bin/safaridriver')
+
+    if executor == 'local':
+        if browser_name == 'chrome':
+            browser = webdriver.Chrome(executable_path=f'{drivers}/chromedriver')
+        elif browser_name == 'firefox':
+            browser = webdriver.Firefox(executable_path=f'{drivers}/geckodriver')
+        elif browser_name == 'opera':
+            browser = webdriver.Opera(executable_path=OperaDriverManager().install(), options=options)
+        elif browser_name == 'safari':
+            browser = webdriver.Safari(executable_path='/usr/bin/safaridriver')
+        else:
+            raise ValueError(f'Browser {browser_name} is not supported! Try to use chrome, firefox, opera or safari')
+
     else:
-        raise ValueError(f'Browser {browser_name} is not supported! Try to use chrome, firefox, opera or safari')
+        executor_url = 'http://{executor}:4444/wd/hub'
+        caps = {
+            'browserName': browser_name,
+            'browserVersion': browser_version,
+            'screenResolution': '1280x720',
+            'selenoid:options': {
+                'enableVNC': vnc,
+            },
+            'acceptSslCerts': True,
+            'acceptInsecureCerts': True
+        }
+
+        browser = webdriver.Remote(
+            command_executor=executor_url,
+            desired_capabilities=caps,
+            options=options
+        )
 
     browser.maximize_window()
-    request.addfinalizer(browser.close)
+
+    driver.log_level = log_level
+    driver.logger = logger
+    driver.test_name = request.node.name
+
+    def fin():
+        browser.close()
+        logger.info(f'Тесты "{request.node.name}" закончили прохождение в {datetime.now()}\n')
+
+    request.addfinalizer(fin)
 
     return browser
 
--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,49 +1,56 @@
-from typing import TYPE_CHECKING
-
 import allure
+import logging
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 
-if TYPE_CHECKING:
-    # pylint: disable=unused-import
-    from selenium.webdriver.remote.webdriver import WebDriver
-
 
 class BasePage:
-    def __init__(self, driver: 'WebDriver'):
+    def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(type(self).__name__)
+
+        file_handler = logging.FileHandler('logs.log')
+        file_handler.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s : %(message)s'))
+        self.logger.addHandler(file_handler)
 
     def wait_for_element_to_appear(self, locator: tuple, message: str, timeout: int = 3):
         with allure.step(f'Подождать появления элемента с локатором {locator}'):
+            self.logger.info(f'Ожидание появления элемента с локатором {locator}')
             try:
                 WebDriverWait(self.driver, timeout).until(
                     lambda _driver: self.driver.find_elements(*locator), message=message
                 )
             except TimeoutException as e:
+                self.logger.error(f'Элемент с локатором {locator} не появился')
                 allure.attach(body=self.driver.get_screenshot_as_png(), name='screenshot')
                 raise AssertionError(e.msg)
 
     def wait_for_elements_to_appear(self, locator: tuple, message: str, quantity: int = 1, timeout: int = 3):
         with allure.step(f'Подождать появления элементов с локатором {locator} в количестве {quantity}'):
+            self.logger.info(f'Ожидание появления элементов с локатором {locator} в количестве {quantity}')
             try:
                 WebDriverWait(self.driver, timeout).until(
                     lambda _driver: len(self.driver.find_elements(*locator)) == quantity, message=message
                 )
             except TimeoutException as e:
+                self.logger.error(f'Элементы с локатором {locator} в количестве {quantity} не появились')
                 allure.attach(body=self.driver.get_screenshot_as_png(), name='screenshot')
                 raise AssertionError(e.msg)
 
     def enter_data(self, input_locator: tuple, data: str):
         with allure.step(f'Ввести данные "{data}" в инпут с локатором {input_locator}'):
+            self.logger.info(f'Ввод данных "{data}" в инпут с локатором {input_locator}')
             active_input = self.driver.find_element(*input_locator)
             active_input.click()
             active_input.send_keys(data)
 
     @allure.step('Подтвердить алерт')
     def confirm_alert(self):
+        self.logger.info('Подтверждение алерта')
         alert = self.driver.switch_to.alert
         alert.accept()
 
     def click_on_element(self, locator: tuple):
         with allure.step(f'Кликнуть на элемент с локатором {locator}'):
+            self.logger.info(f'Клик на элемент с локатором {locator}')
             self.driver.find_element(*locator).click()
