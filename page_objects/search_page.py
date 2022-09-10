8efcfb51bc17996412a1fccc1504b6a49443eddd
StormSpirta <deathwingskotinin@gmail.com>
2022-09-10 22:31:30 +0300

    Add test_invalid_search_on_home_page

--- page_objects/search_page.py
+++ page_objects/search_page.py
@@ -9,6 +9,9 @@ from page_objects.base_page import BasePage
 class SearchPage(BasePage):
     SEARCH_INPUT = (By.CSS_SELECTOR, '[id="input-search"]')
     PRODUCT_NAME = (By.CSS_SELECTOR, '[class="product-thumb"] [class="caption"] a')
+    UNSUCCESS_SEARCH_HEADER = (
+        By.XPATH, '//*[@id="product-search"]//*[text()="There is no product that matches the search criteria."]'
+    )
 
     @allure.step('Получить данные из инпута поиска')
     def get_data_from_search_input(self) -> str:
@@ -20,3 +23,9 @@ class SearchPage(BasePage):
     def get_product_names(self) -> List[str]:
         self.logger.info('Получение названий найденных товаров')
         return [product.get_attribute('textContent') for product in self.driver.find_elements(*self.PRODUCT_NAME)]
+
+    def check_unsuccess_search_header(self):
+        self.wait_for_element_to_appear(
+            locator=self.UNSUCCESS_SEARCH_HEADER,
+            message='Заголовок There is no product that matches the search criteria. должен отображаться на странице'
+        )
--- tests/test_search_page.py
+++ tests/test_search_page.py
@@ -34,4 +34,30 @@ def test_search_on_home_page(driver):
     with allure.step('Проверить что найденные продукты соответствуют поисковому запросу'):
         for product_name in search_page.get_product_names():
             if data not in product_name.lower():
+                allure.attach(body=search_page.driver.get_screenshot_as_png(), name='screenshot')
                 raise AssertionError(f'Найденный продукт "{product_name}" не соответствует поисковому запросу "{data}"')
+
+
+@allure.feature('Главная страница')
+@allure.feature('Страница поиска')
+@allure.title('Проверка поиска несуществующего товара с главной страницы')
+def test_invalid_search_on_home_page(driver):
+    """
+    Кейс:
+    - зайти на главную страницу
+    - ввести в инпут поиска запрос несуществующего товара - "non-existent product"
+    - нажать на кнопку поиска
+    Ожидается:
+    - продуктов соответствующих поисковому запросу нет
+    """
+    data = 'non-existent product'
+
+    home_page = HomePage(driver)
+
+    search_page = home_page.header.search_elements(data)
+    search_page.check_unsuccess_search_header()
+
+    with allure.step('Проверить что продуктов соответствующих поисковому запросу нет'):
+        if search_page.get_product_names():
+            allure.attach(body=search_page.driver.get_screenshot_as_png(), name='screenshot')
+            raise AssertionError(f'Продуктов соответствующих поисковому запросу "{data}" не должно быть на странице')
