e3a442f62726d249c35e80c01c3347122512c422
StormSpirta <deathwingskotinin@gmail.com>
2022-09-11 17:07:27 +0300

    Minor improvements

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,6 +1,5 @@
 import allure
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -55,7 +54,7 @@ class BasePage:
     def click_on_element(self, locator: tuple):
         with allure.step(f'Кликнуть на элемент с локатором {locator}'):
             self.logger.info(f'Клик на элемент с локатором {locator}')
-            ActionChains(self.driver).move_to_element(self.driver.find_element(*locator)).click().perform()
+            self.driver.find_element(*locator).click()
 
     def is_ready(self, locator: tuple):
         self.wait_for_element_to_appear(
